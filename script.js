a2f175939075c4a05ef2f75afefcdf244cb5a79b
saadshr <saad20azmm@gmail.com>
2026-04-19 17:53:20 +0200

    update 1

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 /* script.js */
-console.log("Portfolio Loaded");
+const faders = document.querySelectorAll('.fade');
+window.addEventListener('scroll', () => {
+  faders.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100) {
+      el.style.opacity = 1;
+      el.style.transform = "translateY(0)";
+    }
+  });
+});
